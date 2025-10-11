c462ef407e1a4f2d3b09f6af9a8d0ca10269f7f9
plebwick <finleylowe12345@gmail.com>
2025-10-12 00:06:42 +0100

    3

--- main.py
+++ main.py
@@ -1,38 +1,48 @@
-from math import sqrt, atan2, sin, cos, pi, e
-from people import Person
-from random import uniform, randint
-import pygame, inspect
-from sys import exit
-import pyautogui, time
-import pygame.freetype, cProfile, pstats
-from line_profiler import LineProfiler
-from main import world_y_size
+import pygame
 
+from people import Person, Genes
+from simulation import Simulation
+
+from random import uniform, randint
+from math import pi
+from time import time
 clock = pygame.time.Clock()
 
-screen_x = 2560
-screen_y = 1440
-
-world_x_size = 2560
-world_y_size = 1440
 pygame.init()
-screen = pygame.display.set_mode((screen_x,screen_y))
-pygame.display.set_caption("main")
 
-camera_x = screen_x/2
-camera_y = screen_y/2
-zoom = 1
+sim = Simulation()
 
-people = [Person(x = randint(0,world_x_size)
-                 y = randint(0,world_y_size))
+screen = pygame.display.set_mode((sim.screen_x,sim.screen_y))
+pygame.display.set_caption("Evolution Simulation")
+
+people = [Person(x = randint(0,sim.world_x_size),
+                 y = randint(0,sim.world_y_size),
+                 direction = uniform(0,2*pi),
+                 target = None,
+                 genes = Genes(
+                     uniform(0,100),
+                     uniform(0,100),
+                     uniform(0,100),
+                     uniform(0,100),
+                     uniform(0,100),
+                     uniform(0,100),
+                     uniform(0,100)),
+                 age = 0,
+                 postnatal_elapsed = None,
+                 gestation_period = None,
+                 satiety = 1000,
+                 hydrated = 1000,
+                 current_activity = None,
+                 clock = 0)
                  for i in range(300)]
 
-for i in range(500000):
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-
+    
+    #Key Presses
     keys = pygame.key.get_pressed()
 
     move_speed = 5/zoom
@@ -53,17 +63,21 @@ for i in range(500000):
     if keys[pygame.K_r]:
         zoom = 1
         camera_x = screen_x / 2
-        camera_y = screen_y /2
+        camera_y = screen_y / 2
+
+    #Main simulation
+    time = time.time()
+    while time.time() - time < 1/FPS
 
     screen.fill("blue")
 
     for person in people: 
         person_size = 2
-        person_x = ((person.x - camera_x) * zoom) + (screen_x / 2)
-        person_y = ((person.y - camera_y) * zoom) + (screen_y / 2)
+        person_x = ((person.x - camera_x) * zoom) + (sim.screen_x / 2)
+        person_y = ((person.y - camera_y) * zoom) + (sim.screen_y / 2)
         pygame.draw.circle(screen, (255,255,255), (person_x, person_y), person_size*zoom)
 
-    border_rect = pygame.Rect(((-camera_x * zoom) + screen_x/2),((-camera_y * zoom) + screen_y/2),round(world_x_size*zoom),round(world_y_size*zoom))
+    border_rect = pygame.Rect(((-camera_x * zoom) + sim.screen_x/2),((-camera_y * zoom) + sim.screen_y/2),round(sim.world_x_size*zoom),round(sim.world_y_size*zoom))
     pygame.draw.rect(screen, (255,255,255), border_rect, round(5*zoom))
     pygame.display.update()
     clock.tick()
--- simulation.py
+++ simulation.py
@@ -1,8 +1,17 @@
 class Simulation:
-    def __init__(self):
+    def __init__(self):    
         self.people = []
         self.sources = []
         self.graphs = []
+
+        self.screen_x = 2560
+        self.screen_y = 1440
+        self.world_x_size = 2560
+        self.world_y_size = 1440
+        self.camera_x = self.screen_x/2
+        self.camera_y = self.screen_y/2
+        self.zoom = 1
+        
         self.day = 0
         self.year_length = 365
         self.mutation_rate = 1
