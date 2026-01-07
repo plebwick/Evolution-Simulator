c258502f84676d3e20b9687cea835113a4423b5b
plebwick <finleylowe12345@gmail.com>
2026-01-07 16:51:18 +0000

    Merge branch 'main' of https://github.com/plebwick/Evolution-Simulator

--- simulation.py
+++ simulation.py
@@ -1,4 +1,4 @@
-import pygame, inspect
+import pygame, inspect, pyautogui
 from random import randint, uniform
 from math import pi, ceil, cos, sin
 from sources import Source
@@ -8,11 +8,55 @@ from people import Genes
 from collections import defaultdict
 from time import time, perf_counter
 
+
+class Slider:
+    def __init__(self, x, y, w, min, max, colour, attribute, actual, current_value):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.min = min
+        self.max = max
+        self.colour = colour
+        self.attribute = attribute
+        self.actual = actual
+        self.current_value = current_value
+        self.handle_radius = 20
+
+    def draw(self, sim):
+        pygame.draw.rect(sim.screen, self.colour, (self.x, self.y, self.w, 10))
+        handle_x = self.x + (self.current_value - self.min) / (self.max - self.min) * self.w
+        pygame.draw.circle(sim.screen, (255, 255, 255), (int(handle_x), self.y + 5), 6)
+
+    def draw_text(self, sim):
+        sim.draw_text(self.x - 150, self.y+5, self.attribute, (255, 255, 255), "left", 16)
+
+        sim.draw_text(self.x, self.y + 20 , f"Min: {self.min}", (255, 255, 255), "left", 12)
+        sim.draw_text(self.x + self.w - 50, self.y + 20, f"Max: {self.max}", (255, 255, 255), "left", 12)
+
+        sim.draw_text(self.x + self.w / 2, self.y - 10, f"Value: {round(self.current_value, 2)}", (255, 255, 255), size = 16)
+
+    def pulled(self, sim, mouse_x, mouse_y):
+        relative_x = mouse_x - self.x
+
+        if relative_x > -self.handle_radius and relative_x < self.w+self.handle_radius:
+
+            relative_y = mouse_y - self.y
+
+            if relative_y > -self.handle_radius and relative_y < self.handle_radius+10:
+
+                if pygame.mouse.get_pressed()[0]:
+
+                    self.current_value = max(min(self.min + (relative_x / self.w) * (self.max - self.min), self.max), self.min)
+                    setattr(sim, self.actual, self.current_value)
+
 class Simulation:
     def __init__(self):
         self.people = []
         self.sources = []
         self.permanent_sources = []
+        self.sliders = []
+        self.sim_sliders = []
+        self.graph_sliders = []
 
         self.gene_dict = {
             "size": "blue",
@@ -30,6 +74,16 @@ class Simulation:
             "Male/Female":"#FF008C"
         }
 
+        self.fonts = {
+            12: pygame.font.Font("Pompadour.otf", 12),
+            16: pygame.font.Font("Pompadour.otf", 16),
+            24: pygame.font.Font("Pompadour.otf", 24),
+            32: pygame.font.Font("Pompadour.otf", 32),
+            42: pygame.font.Font("Pompadour.otf", 42),
+            48: pygame.font.Font("Pompadour.otf", 48),
+            64: pygame.font.Font("Pompadour.otf", 64)
+        }
+
         self.gene_method = Genes.__init__
         self.genes = inspect.signature(self.gene_method)
 
@@ -40,7 +94,7 @@ class Simulation:
         self.selected_graph = 0
         self.graph_time = self.screen_x
         self.graph_grid_size = 100
-        self.sampling_frequency = 1
+        self.sampling_frequency = 100
         
         self.graph_x_size = round(self.screen_x*0.8,-2)
         self.graph_y_size = round(self.screen_y*0.8,-2)
@@ -51,17 +105,8 @@ class Simulation:
 
         self.events = None
         self.FPS = 60
-        self.world_x_size = self.screen_x*6
-        self.world_y_size = self.screen_y*6
 
-        self.font = pygame.font.Font("Pompadour.otf", 24)
-
-        self.camera_x = self.world_x_size/2
-        self.camera_y = self.world_y_size/2
-        self.zoom =  self.screen_x/self.world_x_size
-        self.move_speed = 20/(self.zoom)
-        self.zoom_speed = 0.05
-        self.selected_person = None
+        self.randomise_people = True
 
         self.grid = defaultdict(list)
         self.grid_size = 400
@@ -71,92 +116,137 @@ class Simulation:
 
         self.day = 0
         self.season = None
-        self.mutation_rate = 1
 
+        #custom person
+        self.size = 0
+        self.speed = 0
+        self.agility = 0
+        self.wander_agility = 0
+        self.vision_range = 0
+        self.vision_angle = 0
+        self.fertility = 0
+        self.virility = 0
+        self.male_chance = 0
+        self.gestation = 0
+
+        #editable
+        self.world_x_size = self.screen_x*6
+        self.world_y_size = self.screen_y*6
+        self.mutation_rate = 1
         self.starting_population = 200
+        self.food_water_chance = 0.5
 
         total = 50000
         self.permanent_sources_number = 100
         self.food_water_size = 0.2
         self.food_max = total
         self.water_max = total
-        self.food_water_chance = 0.5
+
+        self.camera_x = self.world_x_size/2
+        self.camera_y = self.world_y_size/2
+        self.zoom =  self.screen_x/self.world_x_size
+        self.move_speed = 20/(self.zoom)
+        self.zoom_speed = 0.05
+        self.selected_person = None
+
+    def create_sliders(self):
+        self.sliders.append(Slider(325, 400, 300, 50, 500, "yellow", "Starting population", "starting_population", self.starting_population))
+
+        self.sliders.append(Slider(325, 475, 300, 0.1, 5, "green", "Mutation rate", "mutation_rate", self.mutation_rate))
+
+        #self.sliders.append(Slider(325, 475, 300, 200, 100000, "green", "Food max", "food_max", self.food_max))
+        #self.sliders.append(Slider(325, 550, 300, 200, 100000, "purple", "Water max", "water_max", self.water_max))
+        self.sliders.append(Slider(325, 550, 300, 0, 1, "purple", "Food/water chance", "food_water_chance", self.food_water_chance))
+
+        self.sliders.append(Slider(325, 625, 300, 1000, 25000, "#AAAAAA", "World x size", "world_x_size", self.world_x_size))
+        self.sliders.append(Slider(325, 700, 300, 1000, 25000, "#AAAAAA", "World y size", "world_y_size", self.world_y_size))
+
+        self.sliders.append(Slider(1285, 325, 300, 0, 1, "blue", "Size", "size", self.size))
+        self.sliders.append(Slider(1285, 385, 300, 0, 1, "red", "Speed", "speed", self.speed))
+        self.sliders.append(Slider(1285, 445, 300, 0, 2*pi, "yellow", "Agility", "agility", self.agility))
+        self.sliders.append(Slider(1285, 505, 300, 0, 0.2, "#FFD900", "Wander agility", "wander_agility", self.wander_agility))
+        self.sliders.append(Slider(1285, 565, 300, 0, 1000, "green", "Vision range", "vision_range", self.vision_range))
+        self.sliders.append(Slider(1285, 635, 300, 0, 2*pi, "lightgreen", "Vision angle", "vision_angle", self.vision_angle))
+        self.sliders.append(Slider(1285, 695, 300, 0, 1, "indigo", "Fertility", "fertility", self.fertility))
+        self.sliders.append(Slider(1285, 755, 300, 0, 1, "#8C00FF", "Virility", "virility", self.virility))
+        self.sliders.append(Slider(1285, 815, 300, 0, 1, "#FF00AA", "Male chance", "male_chance", self.male_chance))
+        self.sliders.append(Slider(1285, 875, 300, 0, 1000, "#FFAE51", "Gestation", "gestation", self.gestation))
+    
+    #def create_sim_sliders(self):
+        #self.sim_sliders.append(Slider(50, 50, 300, 100, 60000, "orange", "Simulation Speed (FPS)", "FPS", self.FPS))
+        #self.sim_sliders.append(Slider(50, 975, 300, 100, 2000, "cyan", "Graph Time Range", "graph_time", self.graph_time))
+
+    def create_graph_sliders(self):
+        self.graph_sliders.append(Slider(200, 975, 500, 50, self.day, "cyan", "Graph Time Range", "graph_time", self.graph_time))
 
     def create_people(self):
+        if self.randomise_people:
+            self.people = [Person(x = randint(0,round(self.world_x_size)),
+                    y = randint(0,round(self.world_y_size)),
+                    grid = None,
+                    direction = uniform(0,2*pi),
+                    target = None,
+                    mate = None,
+                    alive = True,
+                    sex = "male" if uniform(0,1) > 0.5 else "female",
+                    genes = Genes(
+                        size =           uniform(0.1,1) ,
+                        speed =          uniform(0.1,1) ,
+                        agility =        uniform(0.1,1) * 2*pi,
+                        wander_agility = uniform(0,0.2) ,
+                        vision_range =   uniform(0.1,1) * 1000,
+                        vision_angle =   uniform(0.1,1) * 2*pi,
+                        fertility =      uniform(0.1,1) ,
+                        virility =       uniform(0.1,1) ,
+                        male_chance =    uniform(0.1,1) ,
+                        gestation =      uniform(0.1,1) * 1000
+                    ),
+                    age = randint(0,100),
+                    postnatal = None,
+                    gestational = None,
+                    satiety = 0,
+                    hydrated = 0,
+                    activity = None,
+                    colour = (randint(0,255), randint(0,255), randint(0,255))
+                    )
+                    for _ in range(round(self.starting_population))]
+        else:
+            self.people = [Person(x = randint(0,self.world_x_size),
+                    y = randint(0,self.world_y_size),
+                    grid = None,
+                    direction = uniform(0,2*pi),
+                    target = None,
+                    mate = None,
+                    alive = True,
+                    sex = "male" if uniform(0,1) > 0.5 else "female",
+                    genes = Genes(
+                        self.size,
+                        self.speed,
+                        self.agility,
+                        self.wander_agility,
+                        self.vision_range,
+                        self.vision_angle,
+                        self.fertility,
+                        self.virility,
+                        self.male_chance,
+                        self.gestation
+                    ),
+                    age = randint(0,100),
+                    postnatal = None,
+                    gestational = None,
+                    satiety = 500,
+                    hydrated = 500,
+                    activity = None,
+                    colour = (randint(0,255), randint(0,255), randint(0,255))
+                    )
+                    for _ in range(round(self.starting_population))]
         
-        self.people = [Person(x = randint(0,self.world_x_size),
-                 y = randint(0,self.world_y_size),
-                 grid = None,
-                 direction = uniform(0,2*pi),
-                 target = None,
-                 mate = None,
-                 alive = True,
-                 sex = "male" if uniform(0,1) > 0.5 else "female",
-                 genes = Genes(
-                     size =           uniform(0.1,1) ,
-                     speed =          uniform(0.1,1) ,
-                     agility =        uniform(0.1,1) * 2*pi,
-                     wander_agility = uniform(0,0.2) ,
-                     vision_range =   uniform(0.1,1) * 1000,
-                     vision_angle =   uniform(0.1,1) * 2*pi,
-                     fertility =      uniform(0.1,1) ,
-                     virility =       uniform(0.1,1) ,
-                     male_chance =    uniform(0.1,1) ,
-                     gestation =      uniform(0.1,1) * 1000
-                 ),
-                 age = randint(0,100),
-                 postnatal = None,
-                 gestational = None,
-                 satiety = 0,
-                 hydrated = 0,
-                 activity = None,
-                 colour = (randint(0,255), randint(0,255), randint(0,255))
-                 )
-                 for _ in range(self.starting_population)]
-        """
-        self.people = [Person(x = randint(0,self.world_x_size),
-                 y = randint(0,self.world_y_size),
-                 grid = None,
-                 direction = uniform(0,2*pi),
-                 target = None,
-                 mate = None,
-                 alive = True,
-                 sex = "male" if uniform(0,1) > 0.5 else "female",
-                 genes = Genes(
-                     2,
-                     3,
-                     2,
-                     0.05,
-                     400,
-                     pi,
-                     0.5,
-                     100,
-                     0.5,
-                     1000
-                 ),
-                 age = randint(0,100),
-                 postnatal = None,
-                 gestational = None,
-                 satiety = 500,
-                 hydrated = 500,
-                 activity = None,
-                 colour = (randint(0,255), randint(0,255), randint(0,255))
-                 )
-                 for _ in range(self.starting_population)]
-        """
-
     def create_sources(self):
-        for i in range(self.permanent_sources_number):
-            x = randint(0,self.world_x_size)
-            y = randint(0,self.world_y_size)
-            type = "food" if uniform(0,1) > self.food_water_chance else "water"
-            p = (x,y,type)
-            self.permanent_sources.append(p)
-        
         self.season = "Spring"
         for i in range(50000):
             Source.respawn(self)
             self.day+=1
+    
     def create_graphs(self):
         #Adds all genes to graphs
         for gene in self.genes.parameters:
@@ -314,8 +404,9 @@ class Simulation:
             x2 = (self.world_x_size - self.camera_x) * self.zoom + self.screen_x/2
             pygame.draw.line(self.screen, (255,255,255), (x1,y), (x2, y), 1)
 
-    def draw_text(self, x, y, text, colour = (255, 255, 255), place = "centre"):
-        text = self.font.render(f"{text}",  True, colour)
+    def draw_text(self, x, y, text, colour = (255, 255, 255), place = "centre", size = 24):
+        font = self.fonts[size]
+        text = font.render(f"{text}",  True, colour)
         if place == "centre": rect = text.get_rect(center = (x,y))
         elif place == "left": rect = text.get_rect(midleft = (x,y))
         self.screen.blit(text, rect)
@@ -326,6 +417,22 @@ class Simulation:
         surface.set_alpha(alpha)
         pygame.draw.rect(surface, colour, surface.get_rect(), border_size)
         self.screen.blit(surface, rect)
+
+    def draw_start_screen(self):
+        self.screen.fill("#131729")
+        self.draw_text(self.screen_x/2, 100, "Evolution Simulation", (255,255,255), size = 64)
+        self.draw_text(self.screen_x/2, self.screen_y - 100, "Press SPACE to Start", (200,200,200), size = 48)
+
+        self.draw_text(self.screen_x/4, 250, "Simulation", (255,255,255), size = 42)
+        self.draw_text(3*self.screen_x/4, 250, "Creature Simulation", (255,255,255), size = 42)
+
+        mouse_x = pygame.mouse.get_pos()[0]
+        mouse_y = pygame.mouse.get_pos()[1]
+
+        for slider in self.sliders:
+            slider.draw_text(self)
+            slider.draw(self)
+            slider.pulled(self, mouse_x, mouse_y)
 
     def draw_simulation(self):
         self.screen.fill("#131729")
@@ -382,6 +489,14 @@ class Simulation:
         self.draw_text(50, 100, f"Zoom {round(self.zoom)}", place = "left")
         self.draw_text(50, 140, f"Population {len(self.people)}", place = "left")
         self.draw_text(50, 180, f"Sources Amount {len(self.sources)}", place = "left")
+
+        mouse_x = pygame.mouse.get_pos()[0]
+        mouse_y = pygame.mouse.get_pos()[1]
+
+        for slider in self.sim_sliders:
+            slider.draw_text(self)
+            slider.draw(self)
+            slider.pulled(self, mouse_x, mouse_y)
 
     def draw_hover_ui(self, person):
         person.draw_vision_radius(self)
@@ -476,6 +591,15 @@ class Simulation:
         self.screen.fill("#131729")
         self.draw_grid()
         self.graphs[self.selected_graph].draw(self)
+
+        mouse_x = pygame.mouse.get_pos()[0]
+        mouse_y = pygame.mouse.get_pos()[1]
+
+        self.graph_sliders[0].max = self.day/self.sampling_frequency
+        for slider in self.graph_sliders:
+            slider.draw_text(self)
+            slider.draw(self)
+            slider.pulled(self, mouse_x, mouse_y)
 
     def draw_grid(self):
         for i in range(round(self.graph_x_size/self.graph_grid_size)+1):
